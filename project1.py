9c377066b168035825ba4bd323978cf742923d7d
tylerae <tylere0455@gmail.com>
2022-05-04 13:44:08 -0600

    Co-authored-by: Jeremy Busch <JeremyCBusch@users.noreply.github.com> Co-authored-by: samuelc7 <samuelc7@users.noreply.github.com> Co-authored-by: Ebersole, Becca <may18019@byui.edu> Co-authored-by: Tammy Nolasco <tammyn102@users.noreply.github.com>

--- project1.py
+++ project1.py
@@ -2,7 +2,30 @@
 import pandas as pd 
 import numpy as np
 import altair as alt 
+import seaborn as sns
 #%%
 df_train = pd.read_csv("https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bank.csv")
 df_test = pd.read_csv("https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bank_holdout_test.csv")
-df_train.head()
+df_train.head(
+)
+
+
+#%%
+features = df_train.filter([
+    "education", "marital", "age", "housing" , 
+    "loan", "day_of_week" , "campaign" , "y", 
+    "job" , "default" , "pdays" , "previous"
+]).sample(500)
+
+
+
+# %%
+def handle_graphs(featues):
+    corr = features.drop(columns = 'y').corr()
+    chart2 = sns.heatmap(corr)
+    chart1 = sns.pairplot(features , hue='y')
+    return chart1 and chart2
+
+#%%
+#Call the functions below 
+handle_graphs(features)
